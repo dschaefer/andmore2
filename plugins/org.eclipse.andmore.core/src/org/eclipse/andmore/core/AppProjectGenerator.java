231e12bf213ce6df493c47d0ffb148cfdd5f1a08
Doug Schaefer <cdtdoug@gmail.com>
2016-03-02 15:49:16 -0500

    Migrated to new Templates framework.

--- plugins/org.eclipse.andmore.core/src/org/eclipse/andmore/core/AppProjectGenerator.java
+++ plugins/org.eclipse.andmore.core/src/org/eclipse/andmore/core/AppProjectGenerator.java
@@ -7,76 +7,35 @@
  *******************************************************************************/
 package org.eclipse.andmore.core;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.andmore.core.internal.Activator;
 import org.eclipse.andmore.core.internal.AndroidBuilder;
 import org.eclipse.andmore.core.internal.AndroidClasspathContainer;
-import org.eclipse.andmore.core.internal.ProjectTemplateManifest;
-import org.eclipse.andmore.core.internal.ProjectTemplateManifest.FileTemplate;
-import org.eclipse.andmore.core.internal.TemplateGenerator;
-import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.tools.templates.freemarker.FMProjectGenerator;
+import org.eclipse.tools.templates.freemarker.SourceRoot;
 
-import com.google.gson.Gson;
+public class AppProjectGenerator extends FMProjectGenerator {
 
-public class AppProjectGenerator {
-
-	private static final String templateRoot = "/templates/app/empty"; //$NON-NLS-1$
-
-	private final TemplateGenerator generator = new TemplateGenerator(new Path(templateRoot));
-
-	private final IProject project;
 	private String packageName;
 	private String activityName;
 	private String layoutName;
-
-	private final Map<String, Object> model = new HashMap<>();
-	private ProjectTemplateManifest manifest;
-	private final List<IFile> filesToOpen = new ArrayList<>();
-
-	public AppProjectGenerator(IProject project) {
-		this.project = project;
-	}
-
-	public IProjectDescription getProjectDescription(URI location, IProject[] refProjects) {
-		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-		IProjectDescription description = workspace.newProjectDescription(project.getName());
-		description.setLocationURI(location);
-		if (refProjects != null) {
-			if (refProjects.length > 0) {
-				description.setReferencedProjects(refProjects);
-			}
-		}
-		description.setNatureIds(new String[] { JavaCore.NATURE_ID, AndroidNature.ID });
-		return description;
-	}
 
 	public void setPackageName(String packageName) {
 		this.packageName = packageName;
@@ -90,26 +49,34 @@ public class AppProjectGenerator {
 		this.layoutName = layoutName;
 	}
 
-	public void generate(IProgressMonitor monitor) throws CoreException {
-		SubMonitor subMonitor = SubMonitor.convert(monitor);
+	@Override
+	protected void initProjectDescription(IProjectDescription description) {
+		description.setNatureIds(new String[] { JavaCore.NATURE_ID, AndroidNature.ID });
 
-		// The model for the templates
-		model.put("projectPath", project.getFullPath().toString()); //$NON-NLS-1$
-		model.put("projectName", project.getName()); //$NON-NLS-1$
+		ICommand javaBuilder = description.newCommand();
+		javaBuilder.setBuilderName(JavaCore.BUILDER_ID);
+
+		ICommand androidBuilder = description.newCommand();
+		androidBuilder.setBuilderName(AndroidBuilder.ID);
+		androidBuilder.setBuilding(IncrementalProjectBuilder.AUTO_BUILD, false);
+
+		description.setBuildSpec(new ICommand[] { javaBuilder, androidBuilder });
+	}
+
+	@Override
+	public void generate(Map<String, Object> model, IProgressMonitor monitor) throws CoreException {
+		setBundle(Activator.getPlugin().getBundle());
 		model.put("packageName", packageName); //$NON-NLS-1$
 		model.put("packagePath", packageName.replace('.', '/')); //$NON-NLS-1$
 		model.put("activityName", activityName); //$NON-NLS-1$
 		model.put("layoutName", layoutName); //$NON-NLS-1$
 
-		// load template manifest
-		StringWriter writer = new StringWriter();
-		generator.loadFile("manifest.json", model, writer); //$NON-NLS-1$
-		manifest = new Gson().fromJson(writer.toString(), ProjectTemplateManifest.class);
+		super.generate(model, monitor);
 
-		generateSources(monitor);
+		IProject project = getProject();
 
 		// Do initial code generation from gradle
-		subMonitor.beginTask("Generating initial sources...", 1);
+		monitor.setTaskName("Generating initial sources...");
 		AndroidBuilder.gradleBuild(project, "generateDebugSources", monitor); //$NON-NLS-1$
 
 		// Mark the build and .gradle folders derived
@@ -117,14 +84,15 @@ public class AppProjectGenerator {
 		project.getFolder(".gradle").setDerived(true, monitor); //$NON-NLS-1$
 
 		// Set up Java project
-		addNatures(monitor);
 		IJavaProject javaProject = JavaCore.create(project);
 
 		// Source folders
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		List<SourceRoot> srcRoots = getManifest().getSrcRoots();
 		List<IClasspathEntry> entries = new ArrayList<>();
-		if (manifest.getSrcEntries() != null) {
-			for (String srcEntry : manifest.getSrcEntries()) {
-				IPath srcPath = project.getFolder(srcEntry).getFullPath();
+		if (srcRoots != null) {
+			for (SourceRoot srcRoot : srcRoots) {
+				IPath srcPath = project.getFolder(srcRoot.getDir()).getFullPath();
 				entries.add(JavaCore.newSourceEntry(srcPath));
 			}
 		}
@@ -144,72 +112,4 @@ public class AppProjectGenerator {
 		javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), monitor);
 	}
 
-	public List<IFile> getFilesToOpen() {
-		return filesToOpen;
-	}
-
-	private void addNatures(IProgressMonitor monitor) throws CoreException {
-		if (monitor != null && monitor.isCanceled()) {
-			throw new OperationCanceledException();
-		}
-		if (!project.hasNature(JavaCore.NATURE_ID)) {
-			IProjectDescription description = project.getDescription();
-			String[] prevNatures = description.getNatureIds();
-			String[] newNatures = new String[prevNatures.length + 2];
-			System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-			newNatures[prevNatures.length] = JavaCore.NATURE_ID;
-			newNatures[prevNatures.length + 1] = AndroidNature.ID;
-			description.setNatureIds(newNatures);
-			project.setDescription(description, monitor);
-		} else {
-			if (monitor != null) {
-				monitor.worked(1);
-			}
-		}
-	}
-
-	private void generateSources(IProgressMonitor monitor) throws CoreException {
-		// Generate the files
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IFile fileToShow = null;
-		for (FileTemplate fileTemplate : manifest.getFiles()) {
-			IPath destPath = new Path(fileTemplate.getDest());
-			IProject project = root.getProject(destPath.segment(0));
-			IFile file = project.getFile(destPath.removeFirstSegments(1));
-			if (!fileTemplate.isSkipTemplate()) {
-				generator.generateFile(fileTemplate.getSrc(), model, file, monitor);
-			} else {
-				try {
-					URL url = FileLocator.find(Activator.getPlugin().getBundle(),
-							new Path(templateRoot).append(fileTemplate.getSrc()), null);
-					try (InputStream in = url.openStream()) {
-						TemplateGenerator.createParent(file, monitor);
-						if (file.exists()) {
-							file.setContents(in, true, true, monitor);
-						} else {
-							file.create(in, true, monitor);
-						}
-					}
-				} catch (IOException e) {
-					throw new CoreException(
-							new Status(IStatus.ERROR, Activator.getId(), "Reading file " + fileTemplate.getSrc(), e));
-				}
-			}
-
-			if (fileTemplate.isOpen()) {
-				if (fileTemplate.isShow()) {
-					if (fileToShow != null) {
-						filesToOpen.add(fileToShow);
-					}
-					fileToShow = file;
-				} else {
-					filesToOpen.add(file);
-				}
-			}
-		}
-
-		if (fileToShow != null) {
-			filesToOpen.add(fileToShow);
-		}
-	}
 }
--- plugins/org.eclipse.andmore.ui/src/org/eclipse/andmore/internal/ui/NewAppWizard.java
+++ plugins/org.eclipse.andmore.ui/src/org/eclipse/andmore/internal/ui/NewAppWizard.java
@@ -8,27 +8,20 @@
 package org.eclipse.andmore.internal.ui;
 
 import java.lang.reflect.InvocationTargetException;
-import java.net.URI;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.andmore.core.AppProjectGenerator;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 import org.eclipse.ui.dialogs.WizardNewProjectReferencePage;
-import org.eclipse.ui.ide.undo.CreateProjectOperation;
-import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
 import org.eclipse.ui.wizards.newresource.BasicNewResourceWizard;
 
 public class NewAppWizard extends BasicNewResourceWizard implements INewWizard {
@@ -57,8 +50,6 @@ public class NewAppWizard extends BasicNewResourceWizard implements INewWizard {
 		mainPage.setDescription("Create Android Application Project");
 		this.addPage(mainPage);
 
-		addPage(new NewAppWizardPage());
-
 		// only add page if there are already projects in the workspace
 		if (ResourcesPlugin.getWorkspace().getRoot().getProjects().length > 0) {
 			referencePage = new WizardNewProjectReferencePage("basicReferenceProjectPage");//$NON-NLS-1$
@@ -66,64 +57,44 @@ public class NewAppWizard extends BasicNewResourceWizard implements INewWizard {
 			referencePage.setDescription("Project References");
 			this.addPage(referencePage);
 		}
+	}
 
+	protected String getTemplateManifestPath() {
+		return "templates/app/empty/manifest.xml"; //$NON-NLS-1$
 	}
 
 	@Override
 	public boolean performFinish() {
-		AppProjectGenerator generator = new AppProjectGenerator(mainPage.getProjectHandle());
-		URI location = null;
+		AppProjectGenerator generator = new AppProjectGenerator();
+		generator.setTemplateManifestPath(getTemplateManifestPath());
+		generator.setProjectName(mainPage.getProjectName());
 		if (!mainPage.useDefaults()) {
-			location = mainPage.getLocationURI();
+			generator.setLocationURI(mainPage.getLocationURI());
 		}
-		IProject[] refProjects = null;
-		if (referencePage != null) {
-			refProjects = referencePage.getReferencedProjects();
-		}
-		IProjectDescription description = generator.getProjectDescription(location, refProjects);
+		generator.setPackageName("com.example.emptyapp");
+		generator.setActivityName("MainActivity");
+		generator.setLayoutName("activity_main");
 
-		CreateProjectOperation op = new CreateProjectOperation(description, "Create Android project") {
-			@Override
-			protected void doExecute(IProgressMonitor monitor, IAdaptable uiInfo) throws CoreException {
-				super.doExecute(monitor, uiInfo);
-				generator.setPackageName("com.example.emptyapp");
-				generator.setActivityName("MainActivity");
-				generator.setLayoutName("activity_main");
-				generator.generate(monitor);
-				List<IFile> toOpen = generator.getFilesToOpen();
-				for (IFile file : toOpen) {
-					selectAndReveal(file);
-				}
-			}
-
-			@Override
-			public boolean canUndo() {
-				// Not set up to undo
-				return false;
-			}
-
-			@Override
-			protected ISchedulingRule getExecuteSchedulingRule() {
-				// Lock the workspace. JDT has a magic project it needs to lock.
-				return ResourcesPlugin.getWorkspace().getRoot();
-			}
-		};
+		Map<String, Object> model = new HashMap<>();
 
 		try {
-			getContainer().run(true, true, new IRunnableWithProgress() {
+			getContainer().run(true, true, new WorkspaceModifyOperation() {
 				@Override
-				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-					try {
-						op.execute(monitor, WorkspaceUndoUtil.getUIInfoAdapter(getShell()));
-					} catch (ExecutionException e) {
-						throw new InvocationTargetException(e);
-					}
+				protected void execute(IProgressMonitor monitor)
+						throws CoreException, InvocationTargetException, InterruptedException {
+					monitor.beginTask("Generating project", 1); //$NON-NLS-1$
+					generator.generate(model, monitor);
+					monitor.done();
+				}
+
+				@Override
+				public ISchedulingRule getRule() {
+					return ResourcesPlugin.getWorkspace().getRoot();
 				}
 			});
 		} catch (InterruptedException | InvocationTargetException e) {
 			throw new RuntimeException(e);
 		}
-
 		return true;
 	}
 
